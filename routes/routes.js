be30dae855deefb22b59449e4d96c56f75556ab4
Brigitte Powers PC <reyhn701@gmail.com>
2021-10-10 09:41:59 -0500

    Removed console.logs, debugging, and psuedocode notes; fixed routing for app.delete and restored function

--- routes/routes.js
+++ routes/routes.js
@@ -4,18 +4,15 @@ const uuid = require('uuid');
 
 module.exports = app => {
     app.get('/notes', (req, res) => {
-        console.log('Hit /notes route')
         res.sendFile(path.join(__dirname, '../public/notes.html'))
     });
 
     app.get('/', (req, res) => {
-        console.log('Hit / route')
         res.sendFile(path.join(__dirname, '../public/index.html'))
     });
 
     // return all saved notes as json
     app.get('/api/notes', (req, res) => {
-        console.log('Hit /api/notes route')
         fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
             if (err) throw err;
             const notes = JSON.parse(data);
@@ -42,17 +39,19 @@ module.exports = app => {
 
     // delete
     app.delete('/api/notes/:id', (req, res) => {
-        const uniqueID = req.params.id;
+        const uniqueId = req.params.id;
         fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
             if (err) throw err;
             const notes = JSON.parse(data);
             const notesArr = notes.filter(item => {
-                return item.id !== uniqueID
-            });
-            fs.writeFile('../db/db.json', JSON.stringify(notesArr), (err, data) => {
+                return item.id !== uniqueId
+            })
+
+            const recreate = JSON.stringify(notesArr);
+            fs.writeFile(path.join(__dirname, '../db/db.json'), recreate, (err) => {
                 if (err) throw err; 
-                res.json(notesArr) 
-            });
+            })
+            res.json(notesArr)
         });
     });
 
